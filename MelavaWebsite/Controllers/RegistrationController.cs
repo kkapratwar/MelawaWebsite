356ad86a0fc128dd78b334f8107dd1da7e9eb5cc
kkapratwar <kalyankworkmail@gmail.com>
2016-08-31 10:17:18 +0530

    Adding missing files

--- MelavaWebsite/Controllers/RegistrationController.cs
+++ MelavaWebsite/Controllers/RegistrationController.cs
@@ -154,18 +154,6 @@ namespace MelavaWebsite.Controllers
 
         public ActionResult Register()
         {
-            List<SelectListItem> items = new List<SelectListItem>();
-
-            items.Add(new SelectListItem { Text = "Action", Value = "0" });
-
-            items.Add(new SelectListItem { Text = "Drama", Value = "1" });
-
-            items.Add(new SelectListItem { Text = "Comedy", Value = "2", Selected = true });
-
-            items.Add(new SelectListItem { Text = "Science Fiction", Value = "3" });
-
-            ViewBag.MovieType = items;
-
             return View();
         }
 
@@ -199,9 +187,9 @@ namespace MelavaWebsite.Controllers
                 }
                 catch (Exception ex)
                 {
-                    throw ex;
+                    return RedirectToAction("Index","Error");
                 }
-                //return RedirectToAction("Index");
+                
             }
             return View(persondetails);
         }
